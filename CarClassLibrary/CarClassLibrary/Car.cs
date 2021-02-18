07daaf2dcec9736d756060564e6730d08609fc6d
NANGGI <mqsora@naver.com>
2021-02-18 22:18:32 +0900

    Add files via upload

---
+++ CarClassLibrary/CarClassLibrary/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car()
+        {
+            this.Make = "nothing yet";
+            this.Model = "nothing yet";
+            this.Price = 0.00m;
+        }
+
+        public Car(string a, string b, decimal c)
+        {
+            this.Make = a;
+            this.Model = b;
+            this.Price = c;
+        }
+
+        override public string ToString()
+        {
+            return "Make : " + Make + " Model : " + Model + " Price : $" + Price;
+        }
+
+    }
+}
