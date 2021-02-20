c1eb7516c33db4a545ec9519e46c1a3367c05c4a
NANGGI <mqsora@naver.com>
2021-02-20 23:19:28 +0900

    Add files via upload

---
+++ LibraryApplication/LibraryApplication/Context/LibraryDb.cs
@@ -0,0 +1,12 @@
+﻿using LibraryApplication.Models;
+using System.Data.Entity;
+
+namespace LibraryApplication.Context
+{
+    public class LibraryDb : DbContext
+    {
+        public LibraryDb() : base("name=DBCS") { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
