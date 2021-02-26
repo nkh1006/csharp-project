77191a094dc4a05dc3dba50d9c29efb56ca3c1a8
NANGGI <mqsora@naver.com>
2021-02-26 23:33:43 +0900

    Add files via upload

---
+++ FTP/FTP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 다운로드를 받을 주소
+            string start = "ftp://addr";
+
+            // 다운로드를 받은 주소
+            string end = "1kb.zip";
+
+            using(WebClient client = new WebClient())
+            {
+                client.DownloadFile(start, end);
+            }
+            
+        }
+    }
+}
